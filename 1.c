d2c93ff93c523edd1d03e40df17a213faf7bae1c
group002 <group002@infoway.com>
2019-12-03 11:53:51 +0530

    initial version

---
+++ 1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+void main()
+{
+	int a, b,result;
+	printf("Enter two numbers:");
+	scanf("%d%d",&a,&b);
+	result = a + b;
+	printf("Result is %d",result);
+}
+
